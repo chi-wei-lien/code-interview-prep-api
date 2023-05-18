535704d31ae68afaf5d03179a67b62eddbcc018d
chi-wei-lien <wilsonwilson3124@gmail.com>
2023-05-18 17:01:48 -0400

    first commit

---
+++ src/main/java/example/CodeInterviewPrepAPI/CodeInterviewPrepApiApplication.java
@@ -0,0 +1,13 @@
+package example.CodeInterviewPrepAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeInterviewPrepApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeInterviewPrepApiApplication.class, args);
+	}
+
+}
